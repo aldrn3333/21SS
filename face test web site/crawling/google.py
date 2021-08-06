8004e60a5d1dd40a4faf6bac2029b35213a8f3cf
aldrn3333 <aldrn3333@naver.com>
2021-08-07 01:24:00 +0900

    remake program

--- face test web site/crawling/google.py
+++ face test web site/crawling/google.py
@@ -3,39 +3,47 @@ from selenium.webdriver.common.keys import Keys
 import time
 import urllib.request
 
-driver = webdriver.Chrome()  # 크롬
-driver.get("https://www.google.co.kr/imghp?hl=ko&tab=ri&ogbl")  # 주소 가져오기
-elem = driver.find_element_by_name("q")  # 검색창
-elem.send_keys("조코딩")  # 검색창 입력
-elem.send_keys(Keys.RETURN)  # 엔터
+driver = webdriver.Chrome()
+driver.get("https://www.google.co.kr/imghp?hl=ko&tab=ri&ogbl")
+elem = driver.find_element_by_name("q")
+elem.send_keys("김수현")
+elem.send_keys(Keys.RETURN)
 
-# 스크롤 다운
 SCROLL_PAUSE_TIME = 1
 
-last_height = driver.execute_script("return document.body.scrollHeight")  # 스크롤 높이
+# Get scroll height
+last_height = driver.execute_script("return document.body.scrollHeight")
 
 while True:
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")  # 스크롤 내림
+    # Scroll down to bottom
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
-    time.sleep(SCROLL_PAUSE_TIME)  # 기다림
+    # Wait to load page
+    time.sleep(SCROLL_PAUSE_TIME)
 
-    new_height = driver.execute_script("return document.body.scrollHeight")  # 새로운 스크롤
+    # Calculate new scroll height and compare with last scroll height
+    new_height = driver.execute_script("return document.body.scrollHeight")
     if new_height == last_height:
-        try:  # 마지막은 예외 처리
+        try:
             driver.find_element_by_css_selector(".mye4qd").click()
         except:
             break
     last_height = new_height
 
-images = driver.find_elements_by_css_selector(".rg_i.Q4LuWd")  # 이미지들 찾기
+images = driver.find_elements_by_css_selector(".rg_i.Q4LuWd")
 count = 1
 for image in images:
     try:
-        image.click()  # 이미지 클릭
-        time.sleep(2)  # 쉬고
-        imgUrl = driver.find_element_by_xpath("/html/body/div[2]/c-wiz/div[3]/div[2]/div[3]/div/div/div[3]/div[2]/c-wiz/div/div[1]/div[1]/div[2]/div[1]/a/img").get_attribute("src")  # 사진 src
-        urllib.request.urlretrieve(imgUrl, str(count) + ".jpg")  # 사진 저장
-        count += 1
+        image.click()
+        time.sleep(2)
+        imgUrl = driver.find_element_by_xpath("/html/body/div[2]/c-wiz/div[3]/div[2]/div[3]/div/div/div[3]/div[2]/c-wiz/div/div[1]/div[1]/div[2]/div[1]/a/img").get_attribute("src")
+        opener = urllib.request.build_opener()
+        opener.addheaders = [('User-Agent', 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1941.0 Safari/537.36')]
+        urllib.request.install_opener(opener)
+        urllib.request.urlretrieve(imgUrl, str(count) + ".jpg")
+        count = count + 1
+        if count == 51:
+            break
     except:
         pass
 
